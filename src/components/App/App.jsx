4d0659ffa729fd2ed4bb84cf8b9043aadb7800ce
Ivan <serene.ease@gmail.com>
2024-01-18 09:51:35 +0200

    reflections ...

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState } from 'react';
+// import { Options } from '../Options/Options';
 
 export const App = () => {
   const [feedback, setFeedback] = useState({
@@ -6,9 +7,41 @@ export const App = () => {
     netural: 0,
     bad: 0,
   });
+
+////////////////////////////////
+const handleFeedback = feedback => {
+  setFeedback({ ...feedback, feedType: feedback.feedType + 1 });
 };
 
-const changeFeedback = () => {
-  setFeedback({ ...feedback, feedType[0]: feedType[1] + 1 });
+const handleFeedButtons = (type) => {setFeedback(type)}
+
+//////////////////////////////
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+const onGood = () => {
+  setFeedback(good);
 };
 
+const onNeutral = () => {
+  setFeedback(neutral);
+};
+
+const onBad = () => {
+  setFeedback(bad);
+};
+////////////////////////////////
+
+
+const Options = ({ onGood, onNeutral, onBad }) => {
+  <div>
+    <button onClick={onGood}>Good</button>
+    <button onClick={onNeutral}>Neutral</button>
+    <button onClick={onBad}>Bad</button>
+  </div>;
+};
+
+return (<h1>Sip Happens Café</h1>
+<p>Please leave your feedback about our service by selecting one of the options below.</p>
+<p></p>
+<p></p>
+<p></p>);
+};
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,7 +1,1 @@
-import { OptionsButton } from '../OptionsButton/OptionsButton';
 
-export const Options = () => {
-  <OptionsButton feedback={feedback} key="good" />;
-  <OptionsButton feedback={feedback} key="neutral" />;
-  <OptionsButton feedback={feedback} key="bad" />;
-};
